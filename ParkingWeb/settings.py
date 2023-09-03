62d4b243e22c341b25e7ac40c9c8ce2205874515
jey-027 <jdrodriguezg25@hotmail.com>
2023-09-02 22:13:27 -0500

    add config db

--- ParkingWeb/settings.py
+++ ParkingWeb/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'ParkingWeb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env('PST_NAME'),
+        'USER': env('PST_USER'),
+        'PASSWORD': env('PST_PASSWORD'),
+        'HOST': env('PST_HOST'),
+        'PORT': env('PST_PORT'),
     }
 }
 
